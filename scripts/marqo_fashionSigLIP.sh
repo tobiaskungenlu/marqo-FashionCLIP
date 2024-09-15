0ea7609839bbbfa1fdfbc3d620983476be1aa254
Wentao Lu <467014858@qq.com>
2024-09-15 12:03:57 +1000

    test

--- scripts/marqo_fashionSigLIP.sh
+++ scripts/marqo_fashionSigLIP.sh
@@ -1,8 +1,9 @@
 MODEL_NAME=Marqo/marqo-fashionSigLIP
 
-for DATASET in 'deepfashion_inshop' 'deepfashion_multimodal' 'fashion200k' 'KAGL' 'atlas' 'polyvore' 'iMaterialist'
+# for DATASET in 'deepfashion_inshop' 'deepfashion_multimodal' 'fashion200k' 'KAGL' 'atlas' 'polyvore' 'iMaterialist'
+for DATASET in 'deepfashion_inshop'
 do
-    python eval.py \
+    python3 eval.py \
         --dataset-config ./configs/${DATASET}.json \
         --model-name ${MODEL_NAME} \
         --run-name Marqo-FashionSigLIP
